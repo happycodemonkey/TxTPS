2c20c30988f54ec16ae0f9b349300948530e1011
happycodemonkey <cmarnold2@gmail.com>
2012-03-26 10:00:07 -0500

    added src folder

---
+++ html/src/translations/ricoLocale_de.js
@@ -0,0 +1,121 @@
+﻿/*****************************************************************
+ Page : ricoLocale_de.js
+ Description : localization strings for german
+ Initial translator: rainer@langheiter@.com
+Version 0.1
+ If you have better translations, or would like to include
+ translations for another language, please send them to dowdybrown@yahoo.com
+******************************************************************/
+RicoTranslate.langCode='fr';
+
+// used in ricoLiveGrid.js
+
+RicoTranslate.addPhraseId('bookmarkExact',"Zeige Einträge $1 - $2 von $3");
+RicoTranslate.addPhraseId('bookmarkAbout',"Zeige Einträge $1 - $2 von ungefähr $3");
+RicoTranslate.addPhraseId('bookmarkNoRec',"Keine Einträge");
+RicoTranslate.addPhraseId('bookmarkNoMatch',"Keine übereinstimmenden Einträge");
+RicoTranslate.addPhraseId('bookmarkLoading',"Lade...");
+RicoTranslate.addPhraseId('sorting',"das Sortieren ist in Bewegung...");
+RicoTranslate.addPhraseId('exportStatus',"Exportieren der Reihe $1");
+RicoTranslate.addPhraseId('filterAll',"(alle)");
+RicoTranslate.addPhraseId('filterBlank',"(freier Raum)");
+RicoTranslate.addPhraseId('filterEmpty',"(leer)");
+RicoTranslate.addPhraseId('filterNotEmpty',"(nicht leer)");
+RicoTranslate.addPhraseId('filterLike',"wie: $1");
+RicoTranslate.addPhraseId('filterNot',"nicht: $1");
+RicoTranslate.addPhraseId('requestError',"Die Anfrage ergab einen Fehler:\n$1");
+RicoTranslate.addPhraseId('keywordPrompt',"Schlüsselwort eintragen, nach dem gesucht werden soll (verwende * als Wildcard):");
+
+// used in ricoLiveGridMenu.js
+
+RicoTranslate.addPhraseId('gridmenuSortBy',"Sortiert nach: $1");
+RicoTranslate.addPhraseId('gridmenuSortAsc',"Aufsteigend");
+RicoTranslate.addPhraseId('gridmenuSortDesc',"Absteigend");
+RicoTranslate.addPhraseId('gridmenuFilterBy',"Gefiltert nach: $1");
+RicoTranslate.addPhraseId('gridmenuRefresh',"erneuern Sie");
+RicoTranslate.addPhraseId('gridmenuChgKeyword',"Schlüsselwort ändern...");
+RicoTranslate.addPhraseId('gridmenuExcludeAlso',"Diesen Wert ebenfalls ausschließen");
+RicoTranslate.addPhraseId('gridmenuInclude',"Nur diesen Wert einbeziehen");
+RicoTranslate.addPhraseId('gridmenuGreaterThan',"Größer oder gleich diesem Wert");
+RicoTranslate.addPhraseId('gridmenuLessThan',"Kleiner oder gleich diesem wert");
+RicoTranslate.addPhraseId('gridmenuContains',"Enthält Schlüsselwort...");
+RicoTranslate.addPhraseId('gridmenuExclude',"Diesen Wert ausschließen");
+RicoTranslate.addPhraseId('gridmenuRemoveFilter',"Entferne Filter");
+RicoTranslate.addPhraseId('gridmenuRemoveAll',"Entfernen Sie alle Filter");
+
+RicoTranslate.addPhraseId('gridmenuExport',"Druck/Exportiert");
+RicoTranslate.addPhraseId('gridmenuExportVis2Web',"Sichtbare Zeilen auf eine Webseite");
+RicoTranslate.addPhraseId('gridmenuExportAll2Web',"Alle Zeilen auf eine Webseite");
+RicoTranslate.addPhraseId('gridmenuExportVis2SS',"Sichtbare Zeilen in einen Verteilungsbogen");
+RicoTranslate.addPhraseId('gridmenuExportAll2SS',"Alle Zeilen in einen Verteilungsbogen");
+
+RicoTranslate.addPhraseId('gridmenuHideShow',"Verbergen/Zeige");
+RicoTranslate.addPhraseId('gridmenuChooseCols',"Wählen Sie die Spalten...");
+RicoTranslate.addPhraseId('gridmenuHide',"Verbergen: $1");
+RicoTranslate.addPhraseId('gridmenuShow',"Zeige: $1");
+RicoTranslate.addPhraseId('gridmenuShowAll',"Alle zeigen");
+
+// used in ricoLiveGridAjax.js
+
+RicoTranslate.addPhraseId('sessionExpireMinutes',"Minuten, bevor Ihre Sitzung abläuft");
+RicoTranslate.addPhraseId('sessionExpired',"ABGELAUFEN");
+RicoTranslate.addPhraseId('requestTimedOut',"Zeitüberschreitung");
+RicoTranslate.addPhraseId('waitForData',"Warte auf Daten...");
+RicoTranslate.addPhraseId('httpError',"Die Anfrage ergab einen Fehler HTTP: $1");
+RicoTranslate.addPhraseId('invalidResponse',"Der Server brachte eine unzulässige Antwort zurück");
+
+// used in ricoLiveGridCommon.js
+
+RicoTranslate.addPhraseId('gridChooseCols',"Wählen Sie die Spalten");
+RicoTranslate.addPhraseId('exportComplete',"der Export ist komplett");
+RicoTranslate.addPhraseId('exportInProgress',"der Export ist in Bewegung...");
+RicoTranslate.addPhraseId('showFilterRow',"zeigen Sie die Filterlinie");
+RicoTranslate.addPhraseId('hideFilterRow',"Verbergen Sie die Filterlinie");
+
+// used in ricoLiveGridForms.js
+
+RicoTranslate.addPhraseId('selectNone',"(keine)");
+RicoTranslate.addPhraseId('selectNewVal',"(neuer Wert)");
+RicoTranslate.addPhraseId('record',"die Eintragung");
+RicoTranslate.addPhraseId('thisRecord',"diesen Eintrag");
+RicoTranslate.addPhraseId('confirmDelete',"Möchten Sie dies wirklich löschen $1?");
+RicoTranslate.addPhraseId('deleting',"die Auslassung ist in Bewegung...");
+RicoTranslate.addPhraseId('formPleaseEnter',"Bitte eingeben einen Wert für $1");
+RicoTranslate.addPhraseId('formInvalidFmt',"unzulässiges Format an $1");
+RicoTranslate.addPhraseId('formOutOfRange',"Wert ist aus Strecke heraus an $1");
+RicoTranslate.addPhraseId('formNewValue',"neuer Wert:");
+RicoTranslate.addPhraseId('saving',"einspeichernd...");
+RicoTranslate.addPhraseId('clear',"Löschtaste");
+RicoTranslate.addPhraseId('close',"Schließen");
+RicoTranslate.addPhraseId('saveRecord',"Speichere");
+RicoTranslate.addPhraseId('cancel',"Abbrechen");
+RicoTranslate.addPhraseId('editRecord',"Bearbeite Sie diese Eintragung");
+RicoTranslate.addPhraseId('deleteRecord',"Löschen Sie diese Eintragung");
+RicoTranslate.addPhraseId('cloneRecord',"Kopieren Sie diese Eintragung");
+RicoTranslate.addPhraseId('addRecord',"Erstellen Sie eine neue Eintragung");
+RicoTranslate.addPhraseId('addedSuccessfully',"$1 wurde erfolgreich addiert");
+RicoTranslate.addPhraseId('deletedSuccessfully',"$1 wurde erfolgreich gelöscht");
+RicoTranslate.addPhraseId('updatedSuccessfully',"$1 wurde erfolgreich aktualisiert");
+
+// used in ricoTree.js
+
+RicoTranslate.addPhraseId('treeSave',"Speichere Auswahl");
+RicoTranslate.addPhraseId('treeClear',"löschen Sie alle");
+
+// used in ricoCalendar.js
+
+RicoTranslate.addPhraseId('calToday',"Heutiger Tag ist $1 $2 $3");
+RicoTranslate.addPhraseId('calWeekHdg',"Woche");
+RicoTranslate.addPhraseId('calYearRange',"Jahr ($1-$2)");
+RicoTranslate.addPhraseId('calInvalidYear',"unzulässiges Jahr");
+
+// Date & number formats
+
+RicoTranslate.monthNames=["Januar", "Februar", "März", "April", "Mai","Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
+RicoTranslate.dayNames=["Sonntag", "Montag","Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"]
+
+RicoTranslate.thouSep="."
+RicoTranslate.decPoint=","
+RicoTranslate.dateFmt="dd.mm.yyyy"
+
+Rico.includeLoaded('ricoLocale_de.js');
